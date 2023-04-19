8894c587ba3e319b6ca195521d0faf89f71ef1d6
Rfalco <falco.encode@gmail.com>
2023-04-19 11:03:51 -0300

    Atualização 19/04/2023

---
+++ developer/site_grconsult/_assets/js/script.js
@@ -0,0 +1,11 @@
+let ul = document.querySelector('nav .ul');
+
+function openMenu() {
+
+    // alert('open aqui');
+    ul.classList.add('open');
+}
+function closeMenu() {
+    // alert('close aqui')
+    ul.classList.remove('open');
+}
